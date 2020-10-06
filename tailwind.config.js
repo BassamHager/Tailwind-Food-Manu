9e6cfb01f4eb50e8ac94713c94e6d4f25506ab6c
BassamHajar <b2dndr1@gmail.com>
2020-10-06 15:56:36 +0200

    updated tailwind config file and other minor changes

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'blue',
+        secondary: {
+          100: 'skyblue',
+          200: 'darkblue'
+        }
+      },
+      fontFamily: {
+        body: ['Sansita+Swashed']
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
